0503e3a1df4ff0f3932f7f6c29e94fbe69e99fbf
LoysGibertoni <loys.henrique@gmail.com>
2015-12-11 17:08:16 -0200

    Soma de pontos

--- block_game_points.php
+++ block_game_points.php
@@ -2,25 +2,71 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-class block_game_points extends block_base {
+class block_game_points extends block_base
+{
 
-    public function init() {
+    public function init()
+	{
         $this->title = get_string('title', 'block_game_points');
     }
 
-    public function get_content() {
-		if ($this->content !== null) {
-			return $this->content;
-		}
-
+    public function get_content()
+	{
+		global $DB, $USER;
 		$this->content = new stdClass;
-		$this->content->text = 'Conteúdo';
-		$this->content->footer = 'Rodapé';
+	
+		if($this->page->course->id == 1) // Pagina inicial
+		{
+			$sql = "SELECT sum(p.points) as points
+				FROM
+					{points_log} p
+				INNER JOIN {logstore_standard_log} l ON p.logid = l.id
+				WHERE l.userid = :userid
+				GROUP BY l.userid";	
 
+			$params['userid'] = $USER->id;
+
+			$points = $DB->get_record_sql($sql, $params);
+			
+			if(empty($points))
+			{
+				$points = new stdClass();
+				$points->points = 0;
+			}
+			
+			$this->content->text = 'Seus pontos: <br><p align="center"><font size="28">' . $points->points . '</font></center>';
+			//$this->content->footer = 'Rodapé';
+		}
+		else // Pagina de um curso
+		{
+			$sql = "SELECT sum(p.points) as points
+				FROM
+					{points_log} p
+				INNER JOIN {logstore_standard_log} l ON p.logid = l.id
+				WHERE l.userid = :userid
+					AND l.courseid = :courseid
+				GROUP BY l.userid";	
+
+			$params['userid'] = $USER->id;
+			$params['courseid'] = $this->page->course->id;
+
+			$points = $DB->get_record_sql($sql, $params);
+			
+			if(empty($points))
+			{
+				$points = new stdClass();
+				$points->points = 0;
+			}
+			
+			$this->content->text = 'Seus pontos: <br><p align="center"><font size="28">' . $points->points . '</font></center>';
+			//$this->content->footer = 'Rodapé';
+		}
+		
 		return $this->content;
     }
 
-    public function has_config() {
+    public function has_config()
+	{
         return true;
     }
 }
